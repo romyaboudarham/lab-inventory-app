fcfe9cf694a8dd764a8da3b37107fe721ea1a98d
Liam Y Lehr <liamlylehr@gmail.com>
2025-12-07 16:15:09 -0600

    liam made a commit

---
+++ my-app/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+export default function Dashboard() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-4xl font-bold text-zinc-800 dark:text-zinc-200">
+        Dashboard
+      </h1>
+      <p className="text-xl ml-4 text-zinc-800 dark:text-zinc-200">
+        This is where the suite of tools for the lab can live.
+      </p>
+    </div>
+  );
+}
